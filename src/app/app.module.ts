1106b78508b8584011b301b251f66f2cffaf384e
Ilya Begalko <ilja.begalko@yandex.ru>
2022-11-08 03:55:10 +0600

    add table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -8,10 +9,18 @@ import { MainComponent } from './components/main/main.component';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { TabViewModule } from 'primeng/tabview';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, MainComponent],
-  imports: [BrowserModule, ButtonModule, InputTextModule, TabViewModule],
+  imports: [
+    BrowserModule,
+    ButtonModule,
+    InputTextModule,
+    TabViewModule,
+    HttpClientModule,
+    TableModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,5 +1,5 @@
-import { Component } from "@angular/core";
-
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -7,5 +7,26 @@ import { Component } from "@angular/core";
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent {
-    constructor() {}
+  comics: any[] = [];
+
+  constructor(private http: HttpClient) {
+    this.initComics();
+  }
+
+  initComics() {
+    this.http
+      .get<any>('../../assets/data/spider-man.json')
+      .subscribe((data) => {
+        this.comics = data;
+        console.log(this.comics);
+      });
+  }
+
+  openComics(url: string) {
+    console.log(url);
+    window.open(url, '_blank');
+  }
+  downloadComics(url: string) {
+    console.log(url);
+  }
 }
